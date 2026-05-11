f5153129fe9fb4fa90ca13010417093206bf9c0d
mingy1206 <mingy837@gmail.com>
2026-05-11 16:14:30 +0900

    ♻️ refactor: 사용자 예제 도메인 제거
    - convention 참고용으로 사용하던 user 도메인 코드를 제거
    
    - CLI 인증 도메인과 중복되는 사용자 예제 테스트 제거
    
    Ref: #S14P31A209-45

--- OAuth/src/main/java/io/envio/auth/domain/user/service/oauth/OAuthUserService.java
+++
@@ -1,8 +0,0 @@
-package io.envio.auth.domain.user.service.oauth;
-
-import io.envio.auth.domain.user.entity.User;
-
-public interface OAuthUserService {
-
-	User findOrCreateGithubUser(final String githubId, final String email);
-}
